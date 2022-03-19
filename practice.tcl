30d683e736087f6a4d9fcf2d6b2712cce63df1d6
yzeng74 <yunlinzeng@gatech.edu>
2022-03-18 23:11:54 -0400

    add practice.tcl

---
+++ practice.tcl
@@ -0,0 +1,12 @@
+set hessLog outfile
+
+set natoms 10
+set nbonds 0
+set nangles 0
+set ndiheds 0
+set nimprops 0
+set zmatqm {0}
+set flag "Q"
+set scan {}
+set readOptParam 0
+set typeList {} ;# only for the get_inthessian_kcal proc
